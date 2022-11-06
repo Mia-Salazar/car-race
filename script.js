c9d51acd477251f72b4e5f56220dd44f68dba54b
Mia-Salazar <maria.sgr@gmail.com>
2022-11-06 20:35:03 +0100

    Add score table

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ const scoreFinal = $("#winningScore")[0];
 const raceLines = $("#raceLines")[0];
 const raceSection = $("#race");
 const scoresTable = $("#scores");
+const scoresBody = $("#scoresBody")[0];
+const toggleRaceButton = $("#toggleRaceButton");
 let toggleToStart = true;
+let toggleToRace = true;
 let players = [];
 let winner;
 
@@ -21,20 +24,9 @@ form.on("reset", function (e) {
 	e.preventDefault();
     raceReset();
 });
-
-
-//Inicio de la carrera
-function raceStart() {
-	toggleForm();
-	createRace();
-	//moveCars();
-}
-
-//Resetear la carrera
-function raceReset() {
-	$("input").val("");
-	toggleForm();
-}
+toggleRaceButton.on("click", function () {
+	changeToggleToRace();
+});
 
 //Toggle de iniciar o resetear
 const toggleForm = () => {
@@ -58,7 +50,7 @@ const toggleForm = () => {
 //Crear líneas de competición
 const createRace = () => {
 	for (let i = 1; i <= Number(numberOfPlayers.value); i++){
-		let player = {img: `./img/car${i}.png`, position: 0}
+		let player = {img: `./img/car${i}.png`, position: i, score: 0}
 		let li = document.createElement("li");
 		let img = document.createElement("img");
 
@@ -76,8 +68,8 @@ const createRace = () => {
 const moveCars = () => {
 	players.forEach((player, index) => {
 		const movement = Math.floor(Math.random() * 10) + 1;
-		const totalPosition = players[index].position + movement;
-		players[index].position = totalPosition; 
+		const totalPosition = players[index].score + movement;
+		players[index].score = totalPosition; 
 		$(`#${++index}`).animate({marginLeft: totalPosition});
 		if (!winner && totalPosition >= scoreFinal.value) {
 			winner = true;
@@ -89,5 +81,55 @@ const moveCars = () => {
 	} else {
 		scoresTable.show();
 		raceSection.hide();
+		toggleRaceButton.show();
+		createScoreTable();
 	}
+}
+
+//Crear tablas con puntuaciones
+const createScoreTable = () => {
+	const sortedPlayer = players.sort((a, b) => (a.score > b.score) ? -1 : 1);
+	sortedPlayer.forEach((sortedPlayer, index) => {
+		let tr = document.createElement("tr");
+		let position = document.createElement("td");
+		let player = document.createElement("td");
+		let score = document.createElement("td");
+
+		position.innerHTML = ++index;
+		player.innerHTML = sortedPlayer.position;
+		score.innerHTML = sortedPlayer.score;
+
+		tr.appendChild(position);
+		tr.appendChild(player);
+		tr.appendChild(score);
+		scoresBody.appendChild(tr);
+	});
+
+}
+
+//Toggle ver puntuaciones o carrera
+const changeToggleToRace = () => {
+	if(toggleToRace) {
+		toggleRaceButton[0].innerHTML = "Puntuación";
+		scoresTable.hide();
+		raceSection.show();
+	} else {
+		toggleRaceButton[0].innerHTML = "Carrera";
+		scoresTable.show();
+		raceSection.hide();
+	}
+	toggleToRace = !toggleToRace;
+}
+
+//Inicio de la carrera
+function raceStart() {
+	toggleForm();
+	createRace();
+	moveCars();
+}
+
+//Resetear la carrera
+function raceReset() {
+	$("input").val("");
+	toggleForm();
 }
